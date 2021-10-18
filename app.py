4270d16721e6a4e918c18ef6301e5cb8ea2c2c79
Lydia <lmwright95@outlook.com>
2021-10-18 10:57:36 +0000

    Add comments for reference

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
+# Import operating system
 import os
+# Import Flask
 from flask import (Flask, render_template)
+# Import PyMongo
 from flask_pymongo import PyMongo
+# Set up environment variables
 if os.path.exists("env.py"):
     import env
 
 # Create an instance of Flask
 app = Flask(__name__)
 
-
+# Configure MongoDB variables
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
@@ -22,7 +26,7 @@ def get_groups():
     groups = mongo.db.groups.find()
     return render_template("data.html", groups=groups)
 
-
+# Set up port & IP environment variables
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
         port=int(os.environ.get("PORT")),
