00880570c57f9a79dde8fc4b7938c0cb7adda3b8
Lydia <lmwright95@outlook.com>
2021-10-18 09:53:49 +0000

    Connect Flask and MongoDB in app.py and test configuration in data.html

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 import os
-from flask import Flask
+from flask import (Flask, render_template)
+from flask_pymongo import PyMongo
 if os.path.exists("env.py"):
     import env
 
-
+# Create an instance of Flask
 app = Flask(__name__)
 
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+# Create an instance of PyMongo
+mongo = PyMongo(app)
+
+# Test function to render data.html test page
 @app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/get_groups")
+def get_groups():
+    groups = mongo.db.groups.find()
+    return render_template("data.html", groups=groups)
 
 
 if __name__ == "__main__":
