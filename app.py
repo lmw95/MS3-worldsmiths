c977dc9d20f3f18fc3129aab22c520229d63649f
Lydia <lmwright95@outlook.com>
2021-11-10 16:53:54 +0000

    Remove unused code from app.py

--- app.py
+++ app.py
@@ -14,35 +14,3 @@ if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
         port=int(os.environ.get("PORT")),
         debug=True)
-
-# Render the create group page
-#@app.route("/create_group", methods=["GET", "POST"])
-#def create_group():
-
-#    user = mongo.db.users.find_one({"email": session["user"]})["email"]
-
-#    new_group = {
-#        "group_name": request.form.get("group-name"),
-#        "group_location": request.form.get("group-location"),
-#        "group_description": request.form.get("group-description"),
-#        "group_cover_img_url": request.form.get("group-banner"),
-#        "group_admin": mongo.db.users.find_one({"email": session["user"]})["_id"]
-#    }
-
-#    if request.method == "POST":
-#        mongo.db.groups.insert_one(new_group)
-#        return render_template("group.html", page_title="{{ group.group_name }}", group=group)
-
-#    return render_template("create-group.html", page_title="Create a group",
-#                            user=user)
-
-
-# Render group page
-#@app.route("/group/<group_id>")
-#def group(group_id):
-
-    #members = list(mongo.db.groups.find({"_id": {"$in": ["members"]}}))
-  #  group = mongo.db.groups.find_one({"_id": ObjectId(group_id)})
-
-   # return render_template("group.html", page_title="{{ group.group_name }}",
-     #                       members=members, group=group)
