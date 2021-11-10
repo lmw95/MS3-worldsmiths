04bc909787edc79fa957ae49a3ad19fe6d5043fe
Lydia <lmwright95@outlook.com>
2021-11-10 22:03:27 +0000

    Remove unused code

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -34,23 +34,3 @@ $(".toggle-password").click(function () {
 });
 
 });
-
-
-/*
-  // Visibility button - https://www.youtube.com/watch?v=INhtMA54iMM
-const visibilityBtn = document.getElementById("visibilityBtn");
-visibilityBtn.addEventListener("click", toggleVisibility)
-
-function toggleVisibility() {
-  const passwordInput = document.getElementById("password")
-  if (passwordInput.type === "password") {
-    passwordInput.type = "text"
-    visibilityBtn.classList.remove("fa-lock")
-    visibilityBtn.classList.add("fa-lock-open")
-  } else {
-    passwordInput.type = "password"
-    visibilityBtn.classList.remove("fa-lock-open")
-    visibilityBtn.classList.add("fa-lock")
-  }}
-  
-*/
