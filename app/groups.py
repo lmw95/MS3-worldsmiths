d88c9d0fef24fc734aab3ce4d2e5daa28c320fa1
Lydia <lmwright95@outlook.com>
2021-11-11 21:19:04 +0000

    Move all group related routes to groups.py

--- app/groups.py
+++ app/groups.py
@@ -59,8 +59,6 @@ def group_page(group_id):
         members_of = User.get_user_by_id(user)["groups_member_of"]
         members = list(User.find_users_in_array(group["members"]))
 
-        print(members)
-
         admin = Group.get_group(group_id)["group_admin"]
         admin_fname = User.get_user_by_id(admin)["first_name"]
         admin_lname = User.get_user_by_id(admin)["last_name"]
@@ -69,3 +67,70 @@ def group_page(group_id):
                                 group=group, admin=admin, user=user,
                                 admin_fname=admin_fname, admin_lname=admin_lname,
                                 members_of=members_of, members=members)
+
+# Edit group
+@groups.route("/edit_group/<group_id>", methods=["GET", "POST"])
+def edit_group(group_id):
+    """
+    Renders 'edit-group.html
+    Gets group id
+    Upon submission, adds updated info to a dictionary
+    Updates MongoDB
+    Redirects user to group page
+    """
+
+    group = Group.get_group(group_id)
+
+    if request.method == "POST":
+        updated_info = {
+            "group_name": request.form.get("name"),
+            "group_location": request.form.get("location"),
+            "group_description": request.form.get("description"),
+            "group_cover_img_url": request.form.get("banner")
+        }
+
+        try:
+            Group.edit_group(group_id, updated_info)
+            flash("Group infomation updated!")
+            return redirect(url_for("groups.group_page", group_id=group_id))
+        except Exception as e:
+            print(e)
+
+    return render_template("edit-group.html", group_id=group_id, group=group)
+
+# Join group
+@groups.route("/join_group/<group_id>", methods=["GET", "POST"])
+def join_group(group_id):
+    """
+    Gets session user id
+    Gets's group id
+    Adds group to 'groups_member_of' in user collection
+    Adds user to 'members' in group collection
+
+    """
+    user = User.get_user_id(session["user"])
+    user_id = user
+
+    group = Group.get_group(group_id)["_id"]
+    group_id = group
+
+    if user:
+        User.add_to_list(user_id, "groups_member_of", group_id)
+        Group.add_to_list(group_id, "members", user_id)
+
+    return redirect(url_for('groups.group_page', group_id=group_id)) 
+
+# Delete group
+@groups.route("/delete_group/<group_id>")
+def delete_group(group_id):
+    """
+    Triggers confirmation modal
+    Gets group id
+    Removes group from owner's list
+    Removes group from member's list
+    Removes group from MongoDB
+    Flashes user to confirm deletion
+    Redirects user to their profile page
+    """
+
+    group = Group.get_group(group_id)
