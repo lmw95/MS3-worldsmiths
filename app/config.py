aa5ca3de6087a0901186f57c14df10ed9b0789ce
Lydia <lmwright95@outlook.com>
2021-11-16 16:23:57 +0000

    Integrated SendGrid as per mentor's recommendations to circumvent gmail connection error

--- app/config.py
+++ app/config.py
@@ -8,13 +8,12 @@ class Config:
     Set environment variables so app runs
     """
     # Flask Mail
-    MAIL_SERVER: os.environ.get("MAIL_SERVER")
-    MAIL_PORT: os.environ.get("MAIL_PORT")
-    MAIL_USE_SSL: os.environ.get("MAIL_USE_SSL")
-    MAIL_USERNAME: os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD: os.environ.get("MAIL_PASSWORD")
-    SECURITY_EMAIL_SENDER: os.environ.get("SECURITY_EMAIL_SENDER")
-    MAIL_DEFAULT_SENDER: os.environ.get("MAIL_DEFAULT_SENDER")
+    MAIL_SERVER = os.environ.get("MAIL_SERVER")
+    MAIL_PORT = os.environ.get("MAIL_PORT")
+    MAIL_USE_TSL = os.environ.get("MAIL_USE_TSL")
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY")
+    MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER")
     # MongoDB
     MONGO_DBNAME = os.environ.get("MONGO_DBNAME")
     MONGO_URI = os.environ.get("MONGO_URI")
