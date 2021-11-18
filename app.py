e0c202ad1f2be78f02b0ee41f7801a6e268064fe
Lydia <lmwright95@outlook.com>
2021-11-18 20:29:31 +0000

    Create and route exception error handling for 403, 404 and 405 errors

--- app.py
+++ app.py
@@ -10,6 +10,31 @@ if os.path.exists("env.py"):
 app = run_app()
 
 
+# Error handling
+@app.errorhandler(403)
+def forbidden(e):
+    """
+    403 error
+    """
+    return render_template("403.html"), 403
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """
+    404 error
+    """
+    return render_template("404.html"), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    """
+    500 error
+    """
+    return render_template("errors/500.html"), 500
+
+
 # Environment settings
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
