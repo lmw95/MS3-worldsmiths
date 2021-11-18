a74ca8f65f6497916ff704c3d59db8e8adb74123
Lydia <lmwright95@outlook.com>
2021-11-18 12:46:01 +0000

    Re-established configuration files after refactoring failed

---
+++ app/settings.py
@@ -0,0 +1,20 @@
+import os
+if os.path.exists("env.py"):
+    import env
+
+
+class Settings:
+    """
+    Set environment variables so app runs
+    """
+    # Flask Mail
+    MAIL_SERVER = os.environ.get("MAIL_SERVER")
+    MAIL_PORT = os.environ.get("MAIL_PORT")
+    MAIL_USE_SSL = os.environ.get("MAIL_USE_SSL")
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("SENDGRID_API_KEY")
+    MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER")
+    # MongoDB
+    MONGO_DBNAME = os.environ.get("MONGO_DBNAME")
+    MONGO_URI = os.environ.get("MONGO_URI")
+    SECRET_KEY = os.environ.get("SECRET_KEY")
