f7384fa5051039160692f1229b52a25fe72b1987
Lydia <lmwright95@outlook.com>
2021-11-18 15:42:17 +0000

    Restructure comment class to take arguments

--- app/classes/comment.py
+++ app/classes/comment.py
@@ -5,7 +5,10 @@ class Comment():
     """
     Creates instance of a comment
     """
-    def __init__(self, args, **kwargs):
+    def __init__(self, comment, commenter,
+                time_posted, date_posted, 
+                group_id, reply, reply_to,
+                reply_user, _id=None):
 
         self._id = _id
         self.comment = comment
